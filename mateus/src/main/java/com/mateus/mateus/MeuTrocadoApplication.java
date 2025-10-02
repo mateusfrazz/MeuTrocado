7eb2b9a64665faefbdb09eebb2ac94e64f947347
mateus <mateusfrazao12345@gmail.com>
2025-10-02 15:38:21 -0300

    Criando o model para despesa

---
+++ mateus/src/main/java/com/mateus/mateus/MeuTrocadoApplication.java
@@ -0,0 +1,13 @@
+package com.mateus.mateus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeuTrocadoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeuTrocadoApplication.class, args);
+	}
+
+}
---
+++ mateus/src/test/java/com/mateus/mateus/MeuTrocadoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mateus.mateus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeuTrocadoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
